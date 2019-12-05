10a253ea542fa2e70bfe91ebbf7b7e1771d219fb
sbblog <fx0817@gmail.com>
2019-12-06 00:00:48 +0800

    Update alnp.sh

--- linux/alnp.sh
+++ linux/alnp.sh
@@ -21,30 +21,8 @@ apk update
 # 安装 nginx
 apk add nginx
 rm /etc/nginx/conf.d/default.conf
-cat > /etc/nginx/conf.d/default.conf << EOF
-# This is a default site configuration which will simply return 404, preventing
-# chance access to any other virtualhost.
+wget -P /etc/nginx/conf.d https://raw.githubusercontent.com/sbblog/file/master/linux/conf/default.conf
 
-server {
-        listen 80 default_server;
-        #listen [::]:80 default_server ipv6only=on;
-        server_name _;
-        index index.html index.htm index.php;
-        root /home/wwwroot/default;
-
-        location ~ [^/]\.php(/|$) {
-                fastcgi_index index.php;
-                fastcgi_pass 127.0.0.1:9000;
-                fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-                include fastcgi_params;
-        }
-
-        # You may need this to prevent return 404 recursion.
-        location = /404.html {
-                internal;
-        }
-}
-EOF
 # 安装 php7 和 sqlite数据库
 apk add php7 php7-mysqli php7-pdo_mysql php7-mbstring php7-json php7-zlib php7-gd php7-intl php7-session php7-fpm php7-memcached php7-tokenizer php7-ctype php7-sqlite3 php7-pdo_sqlite
 
@@ -52,13 +30,13 @@ apk add php7 php7-mysqli php7-pdo_mysql php7-mbstring php7-json php7-zlib php7-g
 mkdir /home/wwwroot
 mkdir /home/wwwroot/default
 
-# 重启nginx 并添加开机启动
-/etc/init.d/nginx restart
-rc-update add nginx
-
 # 重启php7 并添加开机启动
 /etc/init.d/php-fpm7 restart
 rc-update add php-fpm7
+
+# 重启nginx 并添加开机启动
+/etc/init.d/nginx restart
+rc-update add nginx
 
 cat > /home/wwwroot/default/index.php << EOF
 <?php phpinfo(); ?>
