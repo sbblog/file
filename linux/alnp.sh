7a8da213d0e86db33b48a951c2426838e8482711
sbblog <fx0817@gmail.com>
2019-12-13 14:51:04 +0800

    Update alnp.sh

--- linux/alnp.sh
+++ linux/alnp.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -e
 #   --------------------------------------------------------------
 #	系统支持: Alpine 3.10
-#	作    者: feixiang
+#	作    者: sbblog
 #	博    客: https://www.sbblog.cn/
 #	开源地址：https://github.com/sbblog/file/linux
 #	基于Alpine一键安装 Nginx+PHP7+Sqlite3 环境 （支持VPS最小内存32M）
 #   --------------------------------------------------------------
 
 echo -e "系统支持: Alpine 3.10"
-echo -e "作    者: feixiang"
+echo -e "作    者: sbblog"
 echo -e "博    客: https://www.sbblog.cn/"
 echo -e "基于Alpine一键安装 Nginx+PHP7+Sqlite3 环境 （支持VPS最小内存32M）"
 echo -e "开源地址：https://github.com/sbblog/file/linux"
