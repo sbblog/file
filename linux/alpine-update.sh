40c9f23277cb28d33a25ed5414cd2c1cd4fe5d93
sbblog <fx0817@gmail.com>
2020-02-21 14:13:22 +0800

    Update alpine-update.sh

--- linux/alpine-update.sh
+++ linux/alpine-update.sh
@@ -3,15 +3,23 @@
 
 apk update
 apk upgrade
+
 sync
+
 rm  /etc/apk/repositories
 cat > /etc/apk/repositories << EOF
 http://dl-cdn.alpinelinux.org/alpine/latest-stable/main
 http://dl-cdn.alpinelinux.org/alpine/latest-stable/community
 EOF
+
 apk update
 apk upgrade
+
 apk add -u tzdata
 cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+apk add openssh
+echo PermitRootLogin yes >> /etc/ssh/sshd_config
+
 sync
 reboot -f
